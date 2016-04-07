a1d1d4732666cb854a721c6da3e6db27b6ab4cda
belfz <marcinbar1@gmail.com>
2016-04-07 20:24:29 +0200

    moved links to App container

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router'
 
 const Profile = ({dispatch, profile = {}}) => {
     return (
         <div>
-            <Link to={'/logout'}>logout</Link>
             <p>Profile: {profile.email}</p>
         </div>    
     );
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router';
 
 class App extends React.Component {
     constructor (props) {
@@ -9,7 +10,20 @@ class App extends React.Component {
     render () {
         return (
             <div>
-                {this.props.children}
+                <ul>
+                    <li>
+                        <Link to={'/logout'}>logout</Link>
+                    </li>
+                    <li>    
+                        <Link to={'/achievements'}>achievements</Link>
+                    </li>
+                    <li>    
+                        <Link to={'/profile'}>profile</Link>
+                    </li>
+                </ul>
+                <div>
+                    {this.props.children}
+                </div>
             </div>
         );
     }

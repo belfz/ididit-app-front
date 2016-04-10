eed261ee48964a5cb2fee5537e7ff01dad4c0560
belfz <marcinbar1@gmail.com>
2016-04-10 11:30:17 +0200

    new achievement form (with own state)

--- src/components/NewAchievement.js
+++ src/components/NewAchievement.js
@@ -2,49 +2,71 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createAchievement } from '../actions/actions';
 
-const NewAchievement = ({createAchievement}) => {
-  let titleInput, contentInput, doneRadio, notDoneRadio;  
-  function submitAchievement (e) {
+class NewAchievement extends React.Component {
+    constructor (props) {
+        super(props);
+        this.createAchievement = props.createAchievement;
+        this.submitAchievement = this.submitAchievement.bind(this);
+        this.onInput = this.onInput.bind(this);
+        this.state = {
+            achievementModel: {}
+        };
+    }
+    
+    submitAchievement (e) {
       e.preventDefault();
-      const achievement = {
-          title: titleInput.value,
-          content: contentInput.value,
-          done: doneRadio.checked
-      };
-      //TODO add validators
-      //TODO if submit button should be disabled, then need some kind of state (like stateful component)
-      createAchievement(achievement);
-  } 
-  return (
-    <div>  
-        <div>new achievement</div>
-        <form onSubmit={submitAchievement}>
-            <div>
-                <label>
-                    <span>title</span>
-                    <input ref={input => titleInput = input} type="text" />
-                </label>
-            </div>
-            <div>
-                <label>
-                    <span>content</span>
-                    <input ref={input => contentInput = input} type="text" />
-                </label>
-                </div>
-            <div>
-                <label>
-                    <span>done?</span>
-                    <input ref={input => doneRadio = input} type="radio" name="isDone"/>done
-                    <input ref={input => notDoneRadio = input} type="radio" name="isDone" defaultChecked />not done
-                </label>
-            </div>
-            <button type="submit">ok</button>
-        </form>
-    </div>  
-  );  
-};
-
-export default NewAchievement;
+      this.createAchievement(Object.assign(this.state.achievementModel, {done: this.refs.doneRadio.checked}));   
+    }
+    
+    validate (model) {
+      if (!model.title || model.title.length < 1 || !model.content || model.content.length < 1) {
+          return false;
+      }
+      return true;
+    }
+    
+    onInput (propertyName) {
+      return (e) => {
+          this.setState({
+              achievementModel: Object.assign(this.state.achievementModel, {[propertyName]: e.target.value})
+          });
+          this.refs.submitBtn.disabled = !this.validate(this.state.achievementModel);
+      }
+    }
+    
+    render () {
+        return (
+            <div>  
+                <div>new achievement</div>
+                <form onSubmit={this.submitAchievement}>
+                    <div>
+                        <label>
+                            <span>title</span>
+                            <input ref="titleInput" type="text" onInput={this.onInput('title')} />
+                            {!this.state.achievementModel.title ? <span>at least 1 character!</span> : <span></span>}
+                        </label>
+                    </div>
+                    <div>
+                        <label>
+                            <span>content</span>
+                            <input ref="contentInput" type="text" onInput={this.onInput('content')} />
+                            {!this.state.achievementModel.content ? <span>at least 1 character!</span> : <span></span>}
+                        </label>
+                        </div>
+                    <div>
+                        <label>
+                            <span>done?</span>
+                            <input ref="doneRadio" type="radio" name="isDone"/>done
+                            <input ref="notDoneRadio" type="radio" name="isDone" defaultChecked />not done
+                        </label>
+                    </div>
+                    <button type="submit" ref="submitBtn" disabled>ok</button>
+                    <pre>{JSON.stringify(this.state)}</pre>
+                </form>
+            </div>  
+        ); 
+    }
+}
 
 export default connect(
     undefined,

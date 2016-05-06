dbe7c05c168c074b02b5d587cd3061d81ba54fb6
belfz <marcinbar1@gmail.com>
2016-05-06 22:48:53 +0200

    code refactoring

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -10,18 +10,19 @@ import Achievements from '../components/Achievements';
 import NewAchievement from '../components/NewAchievement';
 import { logout, getUser, getAchievements } from '../actions/actions';
 
-
 export const store = configureStore();
 
-function requireAuth (action, nextState, replace, done) {
-    store.dispatch(action())
-    .then(() => done())
-    .catch(() => {
+function requireAuth (asyncAction) {
+  return function (nextState, replace, done) {
+    store.dispatch(asyncAction())
+      .then(() => done())
+      .catch(() => {
         browserHistory.replace({
-            pathname: '/login',
-            state: {nextPathname: nextState.location.pathname}
+          pathname: '/login',
+          state: {nextPathname: nextState.location.pathname}
         });
-    });
+      });
+  }
 }
 
 function doLogout (nextState, replace) {
@@ -34,13 +35,13 @@ class Root extends React.Component {
         return (
             <Router history={browserHistory}>
                 <Route path="/" component={App}>
-                    <IndexRoute component={Profile} onEnter={requireAuth.bind(null, getUser)}/>
+                    <IndexRoute component={Profile} onEnter={requireAuth(getUser)}/>
                     <Route path="/login" component={Login} />
                     <Route path="/register" component={Register} />
                     <Route path="/logout" onEnter={doLogout} />
-                    <Route path="/profile" component={Profile} onEnter={requireAuth.bind(null, getUser)}/>
-                    <Route path="/achievements" component={Achievements} onEnter={requireAuth.bind(null, getAchievements)} />
-                    <Route path="/achievements/new" component={NewAchievement} onEnter={requireAuth.bind(null, getAchievements)} />
+                    <Route path="/profile" component={Profile} onEnter={requireAuth(getUser)}/>
+                    <Route path="/achievements" component={Achievements} onEnter={requireAuth(getAchievements)} />
+                    <Route path="/achievements/new" component={NewAchievement} onEnter={requireAuth(getAchievements)} />
                 </Route>
             </Router>        
         ); 

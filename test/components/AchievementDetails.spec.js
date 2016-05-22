1e4d96839c543f0e33fadcae5cf4cfee1fd5ca4b
belfz <marcinbar1@gmail.com>
2016-05-22 00:25:51 -0400

    corrected failing unit test

--- test/components/AchievementDetails.spec.js
+++ test/components/AchievementDetails.spec.js
@@ -4,28 +4,20 @@ import expect from 'expect';
 import { PureAchievementDetails as AchievementDetails } from '../../src/components/AchievementDetails';
 
 describe('<AchievementDetails />', function () {
-  const achievements = [
-      {
+  const singleAchievement = {
         _id: 123,
         title: 'Title123',
         content: 'Content123',
         done: false
-      }
-    ];
+      };
   
   it('should render achievement details', function () {
-    const params = {
-      id: 123
-    };
-    const wrapper = shallow(<AchievementDetails achievements={achievements} params={params} />);
+    const wrapper = shallow(<AchievementDetails singleAchievement={singleAchievement} />);
     expect(wrapper.find('div.achievement-details').length).toBe(1);
   });
   
   it('should not render achievement details', function () {
-    const params = {
-      id: 'noop'
-    };
-    const wrapper = shallow(<AchievementDetails achievements={achievements} params={params} />);
+    const wrapper = shallow(<AchievementDetails singleAchievement={undefined} />);
     expect(wrapper.find('div.achievement-details').length).toBe(0);
   });
 });

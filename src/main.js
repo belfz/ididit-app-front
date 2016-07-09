dcc8242fb4ce2648112c9d75c4fa8fa70495cc25
belfz <marcinbar1@gmail.com>
2016-07-09 16:59:48 +0200

    removed unused import

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './configurations/axiosConfiguration';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { addCat } from './actions/actions';
